68e139310d8867fea290d891b8db84813ba0763d
Evan White <evan.douglas.white@gmail.com>
2018-07-17 23:22:15 -0500

    Updates bin file to hit root user for simplicity of scp.

--- deploy/deploy.sh
+++ deploy/deploy.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
 # copy deploy files to remote server
-sudo scp -i ./ssh/id_rsa -r ../dist/. evanwhite@178.128.78.21:/var/www/evanwhite.info
+sudo scp -i ./ssh/id_rsa -r ../dist/. root@178.128.78.21:/var/www/evanwhite.info
 
