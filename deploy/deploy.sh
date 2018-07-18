17050d86684f6d7c3387269b52ddb27cf64b5e4d
Evan White <evan.douglas.white@gmail.com>
2018-07-18 16:20:46 -0500

    Updates deploy.sh to build the solution then deploy it, and added terminal prompts to describe actions taken.

--- deploy/deploy.sh
+++ deploy/deploy.sh
@@ -1,5 +1,12 @@
 #!/bin/bash
+echo "Building updated website..."
+yarn build
+echo "Build complete. Deploying..."
 
+echo "Copying /deploy folder to server."
+echo "Please provide the passphrase for the relevant private key."
 # copy deploy files to remote server
-sudo scp -i ./ssh/id_rsa -r ../dist/. root@178.128.78.21:/var/www/evanwhite.info
+scp -i ./ssh/id_rsa -r ../dist/. root@178.128.78.21:/var/www/evanwhite.info
+echo "Finished copying /deploy."
 
+echo "Deploy complete. Website updated at: https://www.evanwhite.info"
